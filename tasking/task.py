f318205e102c62991b27bb9cf16de6afc6f4b27c
suquark <zsy9509@mail.ustc.edu.cn>
2016-08-15 04:47:54 +0800

    stop function capability

--- tasking/task.py
+++ tasking/task.py
@@ -5,13 +5,15 @@ tasklist = {}
 
 
 def start(module):
-    if not module.__name__ in tasklist:
+    if not (module.__name__ in tasklist and tasklist[module.__name__].proc.is_alive):
         r = module.requirement
         assert isinstance(r, dict)
         params = dict([(key, query_first(item)) for key, item in r.items()])
         ctx = Value('i', 0)
         tasklist[module.__name__] = ctx
-        Process(target=module.start, args=(ctx,), kwargs=params).start()
+        p = Process(target=module.start, args=(ctx,), kwargs=params)
+        ctx.proc = p
+        p.start()
 
 
 def softsignal(mod_name, v):
